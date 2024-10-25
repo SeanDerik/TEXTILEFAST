06926311b04ab11f405cfc0985edb4ad123043d3
peco0x <160548695+peco0x@users.noreply.github.com>
2024-10-24 21:09:00 -0300

    Logout
    Adicionado o botão de logout, no navbar, está sem o css e tambem, é preciso fazer a rota para a página de login quando é feito o logout.
    ALTERADO O CÓDIGO DA "Navbar.tsx"

--- pastas_textilefast/Front-end/src/components/Navbar.tsx
+++ pastas_textilefast/Front-end/src/components/Navbar.tsx
@@ -2,17 +2,26 @@ import { Homebutton } from "./Homebutton";
 import { Catalogbutton } from "./Catalogbutton";
 import Textilefastlogo from "../assets/textilefastlogo.png";
 import '../styles/Navbar.css';
+import { useNavigate } from 'react-router-dom';
 
+export function Navbar() {
+    const navigate = useNavigate();  // Substitui useHistory por useNavigate
 
-export function Navbar (){
-    return(
-        <><img src={Textilefastlogo} width="20%" className="littlelogo"/>
-        <nav className="navbar">
-            <div className="buttons">
-                <Homebutton />
-                <Catalogbutton />
-            </div>
-        </nav>
+    const handleLogout = () => {
+        localStorage.removeItem('userToken');
+        navigate('/login');  // Usa navigate ao invés de history.push
+    };
+
+    return (
+        <>
+            <img src={Textilefastlogo} width="20%" className="littlelogo" alt="Textile Fast Logo" />
+            <nav className="navbar">
+                <div className="buttons">
+                    <Homebutton />
+                    <Catalogbutton />
+                    <button className="logoutButton" onClick={handleLogout}>Logout</button>
+                </div>
+            </nav>
         </>
     );
 }
