e9b1b1fed61ba84862cb33912698aaa0fe1461b5
Maxsuell Oliveira da Fonseca <holliob.759@gmail.com>
2024-12-04 21:57:13 -0300

    algumas coisas

--- pastas_textilefast/Front-end/src/pages/Home.tsx
+++ pastas_textilefast/Front-end/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { Navbar } from "../components/Navbar";
 import { Destaques } from "../components/Destaques";
-
+import '../styles/index.css'
 export function Home(){
     return(
         <>
